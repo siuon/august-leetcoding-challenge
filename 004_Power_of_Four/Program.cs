bd2a7e33e17fa80183c1b7da58aa3d35d5d02db1
siuon <sotom@sotom.tw>
2020-08-05 22:08:21 +0800

    add day 4, Power of Four, not complete yet :(

---
+++ 004_Power_of_Four/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _004_Power_of_Four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+                Example 1:
+                Input: 16
+                Output: true
+            */
+
+            var num = 16;
+
+            var solution = new Solution();
+
+            Console.WriteLine(solution.IsPowerOfFour(num) == true);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPowerOfFour(int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
