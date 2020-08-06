994e5ffc8db900b811acbe21ff1f317872d4ef70
siuon <sotom@sotom.tw>
2020-08-06 21:15:31 +0800

    add day 5, Add and Search Word Data structure design, not complete yet :(

---
+++ 005_Add_and_Search_Word_Data_structure_design/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace _005_Add_and_Search_Word_Data_structure_design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Design a data structure that supports the following two operations:
+                    void addWord(word)
+                    bool search(word)
+
+                search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . 
+                means it can represent any one letter.
+
+                Example:
+                addWord("bad")
+                addWord("dad")
+                addWord("mad")
+                search("pad") -> false
+                search("bad") -> true
+                search(".ad") -> true
+                search("b..") -> true
+
+                Note:
+                    You may assume that all words are consist of lowercase letters a-z.
+            */
+
+        }
+    }
+
+    public class WordDictionary
+    {
+
+        /** Initialize your data structure here. */
+        public WordDictionary()
+        {
+
+        }
+
+        /** Adds a word into the data structure. */
+        public void AddWord(string word)
+        {
+
+        }
+
+        /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+        public bool Search(string word)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /**
+     * Your WordDictionary object will be instantiated and called as such:
+     * WordDictionary obj = new WordDictionary();
+     * obj.AddWord(word);
+     * bool param_2 = obj.Search(word);
+     */
+}
