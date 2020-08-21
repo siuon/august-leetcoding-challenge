924299187fd31cc659e80a0508657240969303ba
siuon <sotom@sotom.tw>
2020-08-21 22:14:20 +0800

    add day 20, Reorder List, not complete yet :(

---
+++ 020_Reorder_List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Mime;
+using System.Collections.Generic;
+using System;
+
+namespace _020_Reorder_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+                reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+                You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+                Example 1:
+
+                Given 1->2->3->4, reorder it to 1->4->2->3.
+            */
+
+            var head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+
+            var solution = new Solution();
+
+            solution.ReorderList(head);
+        }
+    }
+
+    /*
+     * Definition for singly-linked list.
+     */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public void ReorderList(ListNode head)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
